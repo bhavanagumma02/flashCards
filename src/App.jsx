61f8552a7e47161098a19d20a98384df63e6acf9
bhavanagumma02 <157484281+bhavanagumma@users.noreply.github.com>
2024-12-18 23:04:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import FlashCards from "./FlashCards";
+
+function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+      {/* hello world */}
+    </div>
+  );
+}
+
+export default App;
